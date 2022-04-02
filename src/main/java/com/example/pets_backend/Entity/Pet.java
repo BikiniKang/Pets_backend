fd66cd5c3bb3f25b125bf22d515b9688d71d57a4
Xinyu Kang <u6120911@anu.edu.au>
2022-04-02 16:50:46 +1100

    Create basic entities User and Pet

---
+++ src/main/java/com/example/pets_backend/Entity/Pet.java
@@ -0,0 +1,45 @@
+package com.example.pets_backend.Entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import javax.persistence.*;
+
+@Entity
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long petId;
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    private String petName;
+
+    public Pet(){}
+    public Pet(String petName) {
+        this.petName = petName;
+    }
+
+
+    public Long getPetId() {
+        return petId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+}
