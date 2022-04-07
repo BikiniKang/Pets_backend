4983eda9092d401754d228e0e02845566672c844
Xinyu Kang <u6120911@anu.edu.au>
2022-04-07 15:20:55 +1000

    Restructured entity User

--- src/main/java/com/example/pets_backend/Entity/User.java
+++ src/main/java/com/example/pets_backend/Entity/User.java
@@ -11,29 +11,109 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    private Long uid;
 
-    private String userName;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 16)
+    private String password;
+
+    @Column(name = "firstName", nullable = false, length = 35)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false, length = 35)
+    private String lastName;
+
+    @Column(name = "phone", length = 15)
+    private String phone;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "image")
+    private String image;
 
     @JsonManagedReference
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Pet> petList = new ArrayList<>();
 
+    @JsonManagedReference
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Task> taskList = new ArrayList<>();
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Event> eventList = new ArrayList<>();
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Folder> folderList = new ArrayList<>();
 
     public User(){}
-    public User(String userName) {this.userName = userName;}
 
-
-    public Long getUserId() {
-        return userId;
+    public Long getUid() {
+        return uid;
     }
 
-    public String getUserName() {
-        return userName;
+    public void setUid(Long uid) {
+        this.uid = uid;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 
     public List<Pet> getPetList() {
@@ -43,5 +123,4 @@ public class User {
     public void setPetList(List<Pet> petList) {
         this.petList = petList;
     }
-
 }
