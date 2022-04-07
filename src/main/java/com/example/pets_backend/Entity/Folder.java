3ec10440ea2c928a6d1371f0c3e4a7547762154f
Xinyu Kang <u6120911@anu.edu.au>
2022-04-07 14:31:42 +1000

    Add new entities (draft only)

---
+++ src/main/java/com/example/pets_backend/Entity/Folder.java
@@ -0,0 +1,20 @@
+package com.example.pets_backend.Entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+
+@Entity
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long fdId;
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "uid", nullable = false)
+    private User user;
+
+    private String name;
+}
