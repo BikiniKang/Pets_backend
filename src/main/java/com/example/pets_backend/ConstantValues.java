c1b86a5b1c0699f90629f2ff6fc0cdf549f69097
Xinyu Kang <u6120911@anu.edu.au>
2022-05-19 01:58:42 +1000

    Update default avatar

--- src/main/java/com/example/pets_backend/ConstantValues.java
+++ src/main/java/com/example/pets_backend/ConstantValues.java
@@ -17,8 +17,8 @@ public class ConstantValues {
     public static final String TOKEN_REFRESH = "/token_refresh";
     public static final String REGISTER = "/register";
     public static final Algorithm ALGORITHM = Algorithm.HMAC256(SECRET.getBytes(StandardCharsets.UTF_8));
-    public static final String DEFAULT_IMAGE = "http://dummyimage.com/400x400";
-    public static final String DEFAULT_IMAGE_PET = "http://dummyimage.com/400x400";
+    public static final String DEFAULT_IMAGE = "https://i.ibb.co/b3w8hXF/Png-Item-223968.png";
+    public static final String DEFAULT_IMAGE_PET = "https://i.ibb.co/P6Cz8CS/image-6.png";
     public static final String DEFAULT_ADDRESS = "";
     public static final String DEFAULT_PHONE = "";
     public static final String DEFAULT_EVENT_TYPE = "";
