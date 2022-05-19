affdf698426458358809483764aa6989b2390634
Xinyu Kang <u6120911@anu.edu.au>
2022-05-19 13:51:37 +1000

    Implemented NtfEventRepository

--- src/main/java/com/example/pets_backend/repository/NtfEventRepository.java
+++ src/main/java/com/example/pets_backend/repository/NtfEventRepository.java
@@ -3,6 +3,13 @@ package com.example.pets_backend.repository;
 
 import com.example.pets_backend.entity.NtfEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NtfEventRepository extends JpaRepository<NtfEvent, String> {
+    NtfEvent findByEventId(String eventId);
+
+    @Modifying
+    @Query("delete from NtfEvent where ntfId = ?1")
+    void deleteByNtfId(String ntfId);
 }
