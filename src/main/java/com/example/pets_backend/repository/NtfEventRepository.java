d049b9883251f40b65d54f4cfbc00f92d65c41ec
Xinyu Kang <u6120911@anu.edu.au>
2022-05-20 03:27:48 +1000

    Debug 'mark Notification as done'

--- src/main/java/com/example/pets_backend/repository/NtfEventRepository.java
+++ src/main/java/com/example/pets_backend/repository/NtfEventRepository.java
@@ -5,7 +5,10 @@ import com.example.pets_backend.entity.NtfEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
 public interface NtfEventRepository extends JpaRepository<NtfEvent, String> {
     NtfEvent findByEventId(String eventId);
 
@@ -13,6 +16,7 @@ public interface NtfEventRepository extends JpaRepository<NtfEvent, String> {
     @Query("delete from NtfEvent where ntfId = ?1")
     void deleteByNtfId(String ntfId);
 
+    @Transactional
     @Modifying
     @Query("update NtfEvent n set n.done = true where n.ntfId = ?1")
     void markAsDone(String ntfId);
