49ee875ab80a335a1324abb0f1dd085875968f6b
Xinyu Kang <u6120911@anu.edu.au>
2022-08-13 14:12:54 +1000

    Add scheduled job to update the sample user

--- src/main/java/com/example/pets_backend/ScheduledJobs.java
+++ src/main/java/com/example/pets_backend/ScheduledJobs.java
@@ -1,11 +1,20 @@
 package com.example.pets_backend;
 
+import com.example.pets_backend.entity.Event;
+import com.example.pets_backend.entity.Task;
 import com.example.pets_backend.entity.User;
 import com.example.pets_backend.repository.UserRepository;
 import com.example.pets_backend.service.NtfTaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static com.example.pets_backend.ConstantValues.DATETIME_PATTERN;
 
 @RequiredArgsConstructor
 @Component
@@ -13,8 +22,9 @@ public class ScheduledJobs {
 
     private final UserRepository userRepository;
     private final NtfTaskService ntfTaskService;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATETIME_PATTERN);
 
-    @Scheduled(cron = "0 0 4 * * *")    // repeat 2am everyday
+    @Scheduled(cron = "0 0 4 * * *")    // repeat 4am everyday
     public void ntfUpcomingTasks() {
         for (User user:userRepository.findAll()) {
             ntfTaskService.addTasksNotification(user, false);
@@ -25,6 +35,19 @@ public class ScheduledJobs {
     public void ntfOverdueTasks() {
         for (User user:userRepository.findAll()) {
             ntfTaskService.addTasksNotification(user, true);
+        }
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")    // repeat 0am everyday
+    @Transactional
+    public void updateSampleUser() {
+        User sampleUser = userRepository.findByUid("4EL4hp_qRUYMzzal_G29f");
+        for (Event event:sampleUser.getEventList()) {
+            event.setStartDateTime(LocalDateTime.parse(event.getStartDateTime(), formatter).plusDays(1).format(formatter));
+            event.setEndDateTime(LocalDateTime.parse(event.getEndDateTime(), formatter).plusDays(1).format(formatter));
+        }
+        for (Task task:sampleUser.getTaskList()) {
+            task.setDueDate(LocalDate.parse(task.getDueDate()).plusDays(1).toString());
         }
     }
 }
