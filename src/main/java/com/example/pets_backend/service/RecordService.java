37ecebf122232ec9ba8f4fca57f2127d3f6c4272
Xinyu Kang <u6120911@anu.edu.au>
2022-10-17 18:07:41 +1100

    Removed redundant interfaces

--- src/main/java/com/example/pets_backend/service/RecordService.java
+++ src/main/java/com/example/pets_backend/service/RecordService.java
@@ -2,15 +2,50 @@ package com.example.pets_backend.service;
 
 
 import com.example.pets_backend.entity.Record;
+import com.example.pets_backend.repository.RecordRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
-public interface RecordService {
-    Record save(Record record);
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class RecordService {
 
-    Record findByRecordId(String recordId);
+    private final RecordRepository recordRepository;
 
-    void deleteByRecordId(String recordId);
+    public Record save(Record record) {
+        log.info("New record '{}' saved into database", record.getRecordId());
+        return recordRepository.save(record);
+    }
 
-    List<Record> findAllByUidAndRecordType(String uid, String recordType);
+    public Record findByRecordId(String recordId) {
+        Record record = recordRepository.findByRecordId(recordId);
+        checkRecordInDB(record,recordId);
+        return record;
+    }
+
+    public void deleteByRecordId(String recordId) {
+        Record record = recordRepository.findByRecordId(recordId);
+        checkRecordInDB(record,recordId);
+        recordRepository.deleteByRecordId(recordId);
+    }
+
+    public List<Record> findAllByUidAndRecordType(String uid, String recordType) {
+        return recordRepository.findAllByUidAndRecordType(uid, recordType);
+    }
+
+    private void checkRecordInDB(Record record, String recordId) {
+        if (record == null) {
+            log.error("Record '{}' not found in the database", recordId);
+            throw new EntityNotFoundException("Record '" + recordId + "' not found in database");
+        } else {
+            log.info("Record '{}' found in the database", recordId);
+        }
+    }
 }
--- src/main/java/com/example/pets_backend/service/TaskService.java
+++ src/main/java/com/example/pets_backend/service/TaskService.java
@@ -2,19 +2,61 @@ package com.example.pets_backend.service;
 
 
 import com.example.pets_backend.entity.Task;
+import com.example.pets_backend.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
-public interface TaskService {
-    Task save(Task task);
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class TaskService {
 
-    Task findByTaskId(String taskId);
+    private final TaskRepository taskRepository;
 
-    void deleteByTaskId(String taskId);
+    public Task save(Task task) {
+        log.info("New task '{}' saved into database", task.getTaskId());
+        return taskRepository.save(task);
+    }
 
-    void archive(String taskId);
+    public Task findByTaskId(String taskId) {
+        Task task = taskRepository.findByTaskId(taskId);
+        checkTaskInDB(task, taskId);
+        return task;
+    }
 
-    List<Task> findUpcomingTasks(String uid, String today);
+    public void deleteByTaskId(String taskId) {
+        Task task = taskRepository.findByTaskId(taskId);
+        checkTaskInDB(task, taskId);
+        taskRepository.deleteByTaskId(taskId);
+        log.info("Task '{}' deleted from database", taskId);
+    }
 
-    List<Task> findOverdueTasks(String uid, String today);
+    public void archive(String taskId) {
+        Task task = taskRepository.findByTaskId(taskId);
+        checkTaskInDB(task, taskId);
+        taskRepository.archive(taskId);
+    }
+
+    public List<Task> findUpcomingTasks(String uid, String today) {
+        return taskRepository.findUpcomingTasks(uid, today);
+    }
+
+    public List<Task> findOverdueTasks(String uid, String today) {
+        return taskRepository.findOverdueTasks(uid, today);
+    }
+
+    private void checkTaskInDB(Task task, String taskId) {
+        if (task == null) {
+            log.error("Task '{}' not found in the database", taskId);
+            throw new EntityNotFoundException("Task '" + taskId + "' not found in database");
+        } else {
+            log.info("Task '{}' found in the database", taskId);
+        }
+    }
 }
